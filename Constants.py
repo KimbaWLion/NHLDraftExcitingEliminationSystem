cbc091c7cc0be96f74d171e5159a10d4218326e5
KimbaWLion <merubin14@gmail.com>
2020-06-27 17:59:51 -0700

    Create Constants.py

---
+++ Constants.py
@@ -0,0 +1,62 @@
+_DICT_OF_TEAMS_IN_DRAFT_ORDER_WITH_BALLS_NUM_2019_DOUBLEBALLS = {
+    "OTT": 29,
+    "LAK": 27,
+    "NJD": 25,
+    "DET": 23,
+    "BUF": 21,
+    "NYR": 19,
+    "EDM": 17,
+    "ANA": 15,
+    "VAN": 13,
+    "PHI": 11,
+    "MIN": 9,
+    "CHI": 7,
+    "FLA": 5,
+    "ARI": 3,
+    "MON": 1
+}
+
+_DICT_OF_TEAMS_IN_DRAFT_ORDER_WITH_BALLS_NUM_2019_SINGLEBALLS = {
+    "OTT": 15,
+    "LAK": 14,
+    "NJD": 13,
+    "DET": 12,
+    "BUF": 11,
+    "NYR": 10,
+    "EDM": 9,
+    "ANA": 8,
+    "VAN": 7,
+    "PHI": 6,
+    "MIN": 5,
+    "CHI": 4,
+    "FLA": 3,
+    "ARI": 2,
+    "MON": 1
+}
+
+_DICT_OF_TEAMS_IN_DRAFT_ORDER_WITH_BALLS_NUM_2020_SINGLEBALLS = {
+    "DET": 15,
+    "OTT": 14,
+    "SJS": 13,
+    "LAK": 12,
+    "ANA": 11,
+    "NJD": 10,
+    "BUF": 9,
+    "NHLA": 8,
+    "NHLB": 7,
+    "NHLC": 6,
+    "NHLD": 5,
+    "NHLE": 4,
+    "NHLF": 3,
+    "NHLG": 2,
+    "NHLH": 1
+}
+DICT_OF_TEAMS_IN_DRAFT_ORDER_WITH_BALLS_NUM_EXAMPLE = {
+    "A": 1,
+    "B": 2,
+    "C": 3,
+    "D": 4
+}
+
+# This is the constant that gets used
+DICT_OF_TEAMS_IN_DRAFT_ORDER_WITH_BALLS_NUM = _DICT_OF_TEAMS_IN_DRAFT_ORDER_WITH_BALLS_NUM_2020_SINGLEBALLS
